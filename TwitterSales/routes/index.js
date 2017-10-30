1f74dbfc95ef0fea5ecc391753c95bf1180df4ea
masterjecht <dan_redguard@hotmail.com>
2017-10-31 09:34:42 +1000

    ojki

--- TwitterSales/routes/index.js
+++ TwitterSales/routes/index.js
@@ -16,6 +16,7 @@ var TweetHashtag = [];
 var TweetLastText = [];
 var TweetSimilar = [];
 
+
 // this post occurs when a user enters some next text
 router.post('/', function(req, res, next) {
     // global varibles for the functions loops
@@ -45,6 +46,7 @@ router.post('/', function(req, res, next) {
 // this is the check posts
 router.post('/check', function(req, res, next) {
 // this is where the program will check fro any updates
+var TweetList = [];
 var i = 1;
 var TweetTest = 0;
 StartWaterfall();
@@ -57,8 +59,16 @@ StartWaterfall();
       function checkdata(data) {
         // check if there is data to be compared to
         if (data.statuses.length !== 0){
-        TweetTest = data.statuses[0].id;
+TweetTest = data.statuses[0].id
         // check tweets
+
+        // add all tweet text to list
+        for (var k = 0; k < data.statuses.length; k ++){
+
+            TweetList.push(data.statuses[k].text);
+            console.log(data.statuses[k].text + " ADDED");
+        }
+        console.log(TweetList.length);
         if (TweetID[i] === TweetTest){
         } else {
                 // if there is a change we will update the data
@@ -85,6 +95,24 @@ StartWaterfall();
         Similarity();
         SimlarityWithSelf(data);
         res.render('index',{tags:TweetHashtag, texts:TweetText});
+        }
+      }
+
+      function SimlarityWithSelf(data){
+        var count = 0;
+        var total = 0;
+
+        for (var k = 1; k < data.statuses.length; k ++){
+          for (var u = 0; u < TweetList.length; u ++){
+        var similarRatio = natural.JaroWinklerDistance(data.statuses[k].text,TweetList[u]);
+      //  console.log("the ration between " + data.statuses[k].text + "And "  + TweetList[u] + "is: " + similarRatio);
+      //    total =total + similarRatio;
+        }
+
+      if (k === data.statuses.length - 1){
+        total = similarRatio / total;
+      //  console.log("the ratio from the last " + data.statuses.length + "tweets is : " + total);
+      }
         }
       }
 
@@ -117,6 +145,20 @@ access_token_key: '2365443590-P6ES9JOcV8pn7QVw8S7rez1KBSHa5oxwypAfvHN',
 access_token_secret: '0ibm4ZEGkAl1MGEsylP4WoKgDQFwTklOCYArurGQLF2R4'
 });
 
+var client4 = new Twitter({
+consumer_key: '	hsTvlRPZZBB4TXCWh49pJ1l2E',
+consumer_secret: 'jYYFDZWzKME9Nsg9BA1mjIHKzmIkTgkAaLuksjIlrN1T6PhNWW',
+access_token_key: '925139252228136960-QPayQc6TuDxQnow4aNeyzwUNd3i6dIi',
+access_token_secret: '3GaaJDBQDOZsRg8f1xsJ5AewL96jQCG5RwLfzpp6oRh1W'
+});
+
+var client5 = new Twitter({
+consumer_key: '	RIqQtGUpXwDkklOizk6UirdKs',
+consumer_secret: '0GRSTsRkA4k5wfKnGk2GonzmCBKGoAfxFpFlkdEtBcW8LQkubl',
+access_token_key: '925140284513468416-z9q0XLi6YZXFKrH2RKgN64YIySWlf9f',
+access_token_secret: 'h53KCgbTTwULJbRxt47vlf8atI8piod71pbMX4lwidjlt'
+});
+
 // check if there is a tweetid
     var params = {q:hashtag, count: 100, lang: 'en'};
 
@@ -134,7 +176,19 @@ access_token_secret: '0ibm4ZEGkAl1MGEsylP4WoKgDQFwTklOCYArurGQLF2R4'
                 if (!error) {
                                       callback(tweets);
                   } else {
+                                  console.log("entering tweet account 4");
+                    client4.get('search/tweets', params, function(error, tweets, response) {
+                      if (!error) {
+                                            callback(tweets);
+                        } else {
+                                        console.log("entering tweet account 5");
+                          client5.get('search/tweets', params, function(error, tweets, response) {
+                            if (!error) {
+                                                  callback(tweets);
+                              } else {
 
+                              }
+                        }
                   }
 
               });
@@ -174,23 +228,6 @@ if (TweetText[o] !== TweetText[u]){
                                   }
 }
 
-function SimlarityWithSelf(data){
-  var count = 0;
-  var total = 0;
-  var k = 0;
-  for (k = 1; k < data.statuses.length; k ++){
-  var similarRatio = natural.JaroWinklerDistance(data.statuses[0].text,data.statuses[k].text);
-    total =total + similarRatio;
-
-if (k === data.statuses.length - 1){
-  total = similarRatio / total;
-  console.log("the ratio from the last " + data.statuses.length + "tweets is : " + total);
-}
-  }
-
-
-
-}
 
 
 module.exports = router;
